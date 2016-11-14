817a5b908eb79985d767d929af9e23732d0cadda
Gio <gio@incendiaryblue.com>
2016-11-14 14:49:15 +0200

    Added step 2 fields.

--- resources/assets/js/components/ApplicationForm.vue
+++ resources/assets/js/components/ApplicationForm.vue
@@ -5,7 +5,7 @@
         <!-- START Step 1 -->
 
         <template v-if="showStep == 1">
-            <h2>Step 1</h2>
+            <h2>Step 1: Details of the leaseholder applying to rent the premises</h2>
             <!-- First Name -->
             <label for="first_name">
                 First Name
@@ -30,11 +30,17 @@
                 <input id="password" type="password" name="password" v-model="appForm.password" required>
             </label>
 
+            <!-- TODO: Chekc if ID number has been filled in, make passport number not required and vice versa -->
+
             <!-- SA ID number -->
             <label for="sa_id_number">
                 South African ID Number
                 <input type="text" name="sa_id_number" v-model="appForm.sa_id_number">
             </label>
+
+            <p>
+                <strong>OR</strong>
+            </p>
 
             <!-- passport number -->
             <label for="passport_number">
@@ -56,41 +62,47 @@
 
             <!-- Cellphone Number -->
             <label for="phone_mobile">
-                Cellphone Number
+                Telephone (Mobile)
                 <input type="text" name="phone_mobile" v-model="appForm.phone_mobile" required>
             </label>
 
             <!-- Home telephone number -->
             <label for="phone_home">
-                Home phone number
-                <input type="text" name="phone_home" v-model="appForm.phone_home" required>
+                Telephone (Home)
+                <input type="text" name="phone_home" v-model="appForm.phone_home">
             </label>
 
             <!-- Work telephone number -->
             <label for="phone_work">
-                Work phone numbera
-                <input type="text" name="phone_work" v-model="appForm.phone_work" required>
+                Telephone (Work)
+                <input type="text" name="phone_work" v-model="appForm.phone_work">
             </label>
 
             <!-- Current Address -->
             <label for="current_address">
-                Current Address
-                <input type="text" name="current_address" v-model="appForm.current_address" required>
+                Address where you currently stay
+                <input type="text" name="current_address" v-model="appForm.current_address">
             </label>
 
             <!-- Marital Status -->
             <label for="marital_status">
                 Marital Status
-                <input type="text" name="marital_status" v-model="appForm.marital_status" required>
+                <input type="text" name="marital_status" v-model="appForm.marital_status">
             </label>
+
+            <!-- TODO: Use vbind to check if maritial status is check and then make this required. -->
 
             <!-- Married Type -->
             <label for="married_type">
-                Marriage Type
-                <input type="text" name="married_type" v-model="appForm.married_type" required>
+                If married, please select the appropriate option
+                <select name="married_type" v-model="appForm.married_type">
+                    <option value="In community of property">In community of property</option>
+                    <option value="ANC">ANC</option>
+                    <option value="Accrual System">Accrual System</option>
+                </select>
             </label>
 
-            <button type="submit" class="success button" v-on:click="submitStep(1, $event)">
+            <button type="submit" class="success button" v-on:click="submitStep(1)">
                 Save Step 1
             </button>
 
@@ -103,7 +115,7 @@
             <h2>Step 2</h2>
             <label for="current_property_owner">
                 Are you the owner of the property where you currently stay?
-                <input type="checkbox" name="current_property_owner" v-model="appForm.current_property_owner" required>
+                <input type="checkbox" name="current_property_owner" v-model="appForm.current_property_owner">
             </label>
 
             <template v-if="appForm.current_property_owner == false">
@@ -111,9 +123,41 @@
                     How long have you rented there
                     <input type="text" name="rental_time" v-model="appForm.rental_time" required>
                 </label>
+
+                <label for="rental_amount">
+                    Monthly rental amount
+                    <input type="text" name="rental_amount" v-model="appForm.rental_amount" required>
+                </label>
+
+                <label for="rental_name">
+                    Name of the Rental Agent / Landlord
+                    <input type="text" name="rental_name" v-model="appForm.rental_name" required>
+                </label>
+
+                <label for="rental_phone_home">
+                    Work phone number
+                    <input type="text" name="rental_phone_home" v-model="appForm.rental_phone_home" required>
+                </label>
+
+                <label for="rental_phone_mobile">
+                    Mobile phone number
+                    <input type="text" name="rental_phone_mobile" v-model="appForm.rental_phone_mobile" required>
+                </label>
             </template>
+
+            <button type="submit" class="success button" v-on:click="submitStep(2)">
+                Save Step 2
+            </button>
         </template>
         <!-- END Step 2 -->
+
+        <!-- START Step 3 -->
+
+        <template v-if="showStep == 3">
+            <h2>Step 3</h2>
+        </template>
+
+        <!-- END Step 3 -->
     </form>
 
 </template>
@@ -165,9 +209,7 @@
             testXHR: function() {
             },
 
-            submitStep: function(step, event) {
-
-            console.log("This mew", this.meow);
+            submitStep: function(step) {
 
                 this.$http.post(
                     this.formAction,
