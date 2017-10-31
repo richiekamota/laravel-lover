ccb8de25b9ade3340f0c7ab60fa02f6168e6c371
Richmond <richiekamota@gmail.com>
2017-10-31 13:20:43 +0200

    Dynamic update of the copyright year with carbon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -64,7 +64,7 @@
        <div class="row">
            <div class="columns">
                <div class="page__copyright">
-                   All rights reserved. © 2017
+                   All rights reserved. © {{$year}}
                </div>
            </div>
            <div class="columns">
