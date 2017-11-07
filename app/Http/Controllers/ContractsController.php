7ccbc6d074002de49851d15306022d6190544d23
Richmond <richiekamota@gmail.com>
2017-11-07 07:55:45 +0200

    Added updating the contract table status column to 'declined' when the contract is declined.

--- app/Http/Controllers/ContractsController.php
+++ app/Http/Controllers/ContractsController.php
@@ -421,6 +421,7 @@ class ContractsController extends Controller
             $contract = Contract::findOrFail($id);
             $application = Application::findOrFail($contract->application_id);
             $application->contract_decline_reason = $data['data'];
+            $contact->status = 'declined';
             $application->save();
 
             // ApplicationEvent::create([
